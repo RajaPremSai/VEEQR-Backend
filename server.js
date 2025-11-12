bf72bbb28f64c0338144f9461a6d6d31d1eedf14
RAJA PREM SAI MADDULA <123068364+RajaPremSai@users.noreply.github.com>
2025-11-13 00:14:14 +0530

    Fixed cors policy

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173','https://veeqr-backend.onrender.com'],
+  origin: ['http://localhost:3000', 'http://localhost:5173','https://veeqr-frontend-1.onrender.com'],
   credentials: true
 }));
 
